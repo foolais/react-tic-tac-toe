fa53d639fc3bb06fa4826ea1e04eec4d59216578
wahyu.esya17 <wahyu.esya17@gmail.com>
2023-12-12 20:29:55 +0700

    add additional

--- src/App.jsx
+++ src/App.jsx
@@ -4,70 +4,129 @@ import Box from "./components/Box";
 import Swal from "sweetalert2";
 
 function App() {
-  const [isChangePlayer, setIsChangePlayer] = useState(true);
+  const [isChangePlayer, setIsChangePlayer] = useState(false);
   const [data, setData] = useState(Array(9).fill(null));
   const [isPlaying, setIsPlaying] = useState(false);
   const [winner, setWinner] = useState(null);
+  const [history, setHistory] = useState([]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const [isChangeHistory, setIsChangeHistory] = useState(false);
 
+  // when changes data
   useEffect(() => {
-    setWinner(calculateWinner(data));
+    setWinner(calculateWinner(data, setIsChangePlayer));
   }, [data]);
 
   const handleBoxClick = (value) => {
-    if (!isPlaying || calculateWinner(data) !== null) {
+    const isWinner = calculateWinner(data, setIsChangePlayer);
+
+    if (!isPlaying || isWinner !== null) {
       Swal.fire({
         icon: "warning",
-        text: `The Game Has Ended`,
+        text: `Please Start The Game First`,
         allowOutsideClick: false,
       });
     } else {
+      setIsChangeHistory(false);
+      setCurrentMove((prev) => prev + 1);
       setData((prevData) => {
         const newData = [...prevData];
-        newData[value.index] = isChangePlayer ? 0 : 1;
+        const newHistory = [...history.slice(0, currentMove + 1), newData];
+        newData[value.index] = isChangePlayer ? "O" : "X";
+        setHistory(newHistory);
         setIsChangePlayer(!isChangePlayer);
 
         return newData;
       });
+      if (currentMove === 8)
+        Swal.fire({
+          icon: "warning",
+          text: `Game Over`,
+          allowOutsideClick: false,
+        });
     }
   };
 
-  const resetData = () => {
+  const goToMove = (data) => {
+    setIsChangePlayer(data.index % 2 === 0);
+    setCurrentMove(data.index);
+    setData(data.arr);
+    setIsChangeHistory(true);
+  };
+
+  const resetGame = () => {
+    setHistory([]);
     setIsPlaying(false);
+    setIsChangePlayer(false);
+    setCurrentMove(0);
     setData(Array(9).fill(null));
+    setIsChangeHistory(false);
   };
 
   return (
     <>
-      <h1 className="text-teal-500">Tic Tac Toe {winner}</h1>
-      {!isPlaying ? (
-        <div className="card">
-          <button onClick={() => setIsPlaying(true)}>Start</button>
+      <div className="flex gap-20">
+        <div>
+          <h1 className="text-teal-500">Tic Tac Toe </h1>
+          {!isPlaying ? (
+            <div className="card">
+              <button onClick={() => setIsPlaying(true)}>Start</button>
+            </div>
+          ) : (
+            <div className="card grid">
+              <button onClick={() => resetGame()}>
+                {winner?.data ? "Try Again" : "Reset Game"}
+              </button>
+              {winner?.data ? (
+                <span className="my-2 text-lg">Winner is {winner?.data}</span>
+              ) : (
+                <span className="my-2 text-lg">
+                  Next Player is {isChangePlayer ? "O" : "X"}
+                </span>
+              )}
+            </div>
+          )}
+          <div className="flex">
+            <div className="grid grid-cols-3 pa-0 ma-0 gap-0">
+              {data.map((data, index) => (
+                <Box
+                  key={index}
+                  value={{ index, data }}
+                  winnerData={winner?.box}
+                  handleBoxClick={handleBoxClick}
+                />
+              ))}
+            </div>
+          </div>
         </div>
-      ) : (
-        <div className="card grid">
-          <button onClick={() => resetData()}>
-            {winner ? "Try Again" : "Reset Game"}
-          </button>
-          {winner ? (
-            <span className="my-2 text-lg">Winner is {winner}</span>
-          ) : null}
+        <div className="mt-[13vh] text-left">
+          <h2 className="text-lg font-semibold text-teal-400 mb-3">History</h2>
+          <div className="grid gap-3">
+            {history.map((arr, index) => {
+              return (
+                <div key={index} className="flex gap-2">
+                  <span>{index + 1}. </span>
+                  <button
+                    className={`${
+                      isChangeHistory && currentMove === index
+                        ? "bg-teal-500"
+                        : ""
+                    } `}
+                    onClick={() => goToMove({ arr, index })}
+                  >
+                    <span>Go To Move #{index + 1}</span>
+                  </button>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      )}
-      <div className="grid grid-cols-3 pa-0 ma-0 gap-0">
-        {data.map((data, index) => (
-          <Box
-            key={index}
-            value={{ index, data }}
-            handleBoxClick={handleBoxClick}
-          />
-        ))}
       </div>
-      <br />
     </>
   );
 }
 
-function calculateWinner(data) {
+function calculateWinner(data, setIsChangePlayer) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -87,7 +146,12 @@ function calculateWinner(data) {
         text: `Winner is ${data[a]}`,
         allowOutsideClick: false,
       });
-      return data[a];
+      setIsChangePlayer(false);
+      const payload = {
+        data: data[a],
+        box: [a, b, c],
+      };
+      return payload;
     }
   }
   return null;
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 const Box = (props) => {
-  const { value, handleBoxClick } = props;
+  const { value, handleBoxClick, winnerData } = props;
 
   const onBoxClick = () => {
     if (!value.data) handleBoxClick(value);
@@ -8,10 +8,16 @@ const Box = (props) => {
 
   return (
     <div
-      className="w-24 h-24 border-2 border-sky-100 flex items-center justify-center cursor-pointer"
+      className={`w-24 h-24 border-2 border-sky-100 flex items-center justify-center cursor-pointer ${
+        winnerData && winnerData.includes(value.index)
+          ? "bg-teal-500 border-teal-500"
+          : ""
+      }`}
       onClick={() => onBoxClick()}
     >
-      <span className="text-xl">{value.data}</span>
+      <span className="text-xl">
+        {value.data === "X" ? "X" : value.data === "O" ? "O" : ""}
+      </span>
     </div>
   );
 };
